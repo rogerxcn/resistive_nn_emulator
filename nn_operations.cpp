cc5b5b558cdc6263b81012c787cd8f96716a21f8
陈润锋 <chenrunfeng@chenrunfengdeMacBook-Pro.local>
2019-01-25 14:02:24 -0500

    add bias

--- nn_operations.cpp
+++ nn_operations.cpp
@@ -34,9 +34,9 @@ void conv2d(float* input, float* output, float* filter, int input_size, int num_
 //  bias: a pointer to the memory start location containing the filter matrix (1D)
 //  input_legnth: specifies the overall length of the input (and the bias has the same length)
 void add_bias(float* input, float* output, float* bias, int input_length) {
-
-
-
+    for (int i = 0; i < input_legnth; i++) {
+        output[i] = bias[i] + input[i];
+    }
 }
 
 
